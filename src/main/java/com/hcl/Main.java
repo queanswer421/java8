a6a9a50e5aa5d1bcc91640d0773755c1e75a0c6f
Lukasz Hacas <lukaszhacas@hcl.com>
2022-12-24 13:38:01 +0100

    one one2

--- src/main/java/com/hcl/Main.java
+++ src/main/java/com/hcl/Main.java
@@ -2,6 +2,7 @@ package com.hcl;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String test = "testowanie";
+        System.out.println(test);
     }
 }
