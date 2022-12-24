ab0b3ed122383df5e74bb6f35d5789aed82c874f
Marek Nowak <marek@hcl.com>
2022-12-24 13:41:20 +0100

    marek1

--- src/main/java/com/hcl/Main.java
+++ src/main/java/com/hcl/Main.java
@@ -2,7 +2,7 @@ package com.hcl;
 
 public class Main {
     public static void main(String[] args) {
-        String test = "testowanie";
+        String test = "testowanie...";
         System.out.println(test);
     }
 }
